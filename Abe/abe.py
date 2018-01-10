f81334c91e2565fec4722a229048b1b72464a72b
Scott King <Lvl4Sword@users.noreply.github.com>
2018-01-09 17:03:38 -0700

    This works properly with https://hstspreload.org/

--- Abe/abe.py
+++ Abe/abe.py
@@ -54,7 +54,7 @@ EPOCH1970 = calendar.timegm(TIME1970)
 # Configurable templates may contain either.  HTML seems better supported
 # under Internet Explorer.
 DEFAULT_CONTENT_TYPE = "text/html; charset=utf-8"
-DEFAULT_HOMEPAGE = "chains";
+DEFAULT_HOMEPAGE = "chain";
 DEFAULT_TEMPLATE = """
 <!DOCTYPE html>
 <html lang="en">
