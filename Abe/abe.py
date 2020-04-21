3020248909ba8de1f1ae444a9b478311dfc7c76e
Scott King <Lvl4Sword@users.noreply.github.com>
2020-04-21 01:38:44 -0600

    Properly escape env['PATH_INFO'] to remedy ...
    this nonsense: https://github.com/bitcoin-abe/bitcoin-abe/issues/292

--- Abe/abe.py
+++ Abe/abe.py
@@ -213,7 +213,7 @@ class Abe:
             "body": [],
             "env": env,
             "params": {},
-            "dotdot": "../" * (env['PATH_INFO'].count('/') - 1),
+            "dotdot": "../" * (escape(env['PATH_INFO']).count('/') - 1),
             "start_response": start_response,
             "content_type": abe.template_vars['CONTENT_TYPE'],
             "template": abe.template,
